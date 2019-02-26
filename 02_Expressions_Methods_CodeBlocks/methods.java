b61550e6b2d8214d69472267e3143972170338a1
Mohammed Pasha <mk.pasha@hotmail.com>
2019-02-26 01:44:58 -0500

    Section 2 files
    includes keywords, statements, codeBlocks and methods java files.

---
+++ 02_Expressions_Methods_CodeBlocks/methods.java
@@ -0,0 +1,9 @@
+public class methods {
+  public static void main(String[] args) {
+    System.out.println("-----methods-----");
+
+
+
+    System.out.println("---------------END---------------");
+  }
+}
