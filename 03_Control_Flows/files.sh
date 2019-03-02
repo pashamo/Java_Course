dcd305fc834f15a71c762aa3f2e2a2d422a341d8
Mohammed Pasha <mk.pasha@hotmail.com>
2019-03-02 02:00:08 -0500

    Files.sh commented file concatenation code in order to avoid deletion of existing code

--- 03_Control_Flows/files.sh
+++ 03_Control_Flows/files.sh
@@ -3,13 +3,13 @@
 echo "Creating files..."
 for i in {14..28}
 do
-    echo '/**/
-    public class codingExercise'$i' {
-      public static void main(String[] args) {
-        System.out.println("-----codingExercise'$i'-----");
-        System.out.println("-----END-----");
-      }
-    }' > "codingExercise$i.java"
+#    echo '/**/
+#    public class codingExercise'$i' {
+#      public static void main(String[] args) {
+#        System.out.println("-----codingExercise'$i'-----");
+#        System.out.println("-----END-----");
+#      }
+#    }' > "codingExercise$i.java"
     echo "codingExercise$i.java created"
 done
 echo "Files Created."
