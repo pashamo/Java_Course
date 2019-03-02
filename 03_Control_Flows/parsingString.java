41368e19a1b21d3295e20e2641dde95451c721d8
Mohammed Pasha <mk.pasha@hotmail.com>
2019-03-02 17:52:21 -0500

    parsingString java and class updated

--- 03_Control_Flows/parsingString.java
+++ 03_Control_Flows/parsingString.java
@@ -1,6 +1,21 @@
 public class parsingString {
   public static void main(String[] args) {
     System.out.println("-----parsingString-----");
+
+    String numAsString = "2019";
+    System.out.println(numAsString);
+
+    int stringAsNum = Integer.parseInt(numAsString);
+
+    numAsString += 1;
+    stringAsNum += 1;
+
+    System.out.println(numAsString + " " + stringAsNum);
+
+    double stringAsDbl = Double.parseDouble(numAsString);
+
+    System.out.println(numAsString + " " + stringAsNum + " " + stringAsDbl);
+
     System.out.println("-----END-----");
   }
 }
