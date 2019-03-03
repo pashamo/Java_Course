eb7e5e8c405db74c1c2a96b029eeb40ff85b3be0
Mohammed Pasha <mk.pasha@hotmail.com>
2019-03-03 16:25:08 -0500

    codingExercise28 java and class updated
    Section complete with this exercise.

--- 03_Control_Flows/codingExercise28.java
+++ 03_Control_Flows/codingExercise28.java
@@ -78,6 +78,60 @@ NOTE: Do not add the main method to the solution code.
 public class codingExercise28 {
   public static void main(String[] args) {
     System.out.println("-----codingExercise28-----");
+    System.out.println(getBucketCount(-3.4, 2.1, 1.5, 2));
+    System.out.println(getBucketCount(3.4, 2.1, 1.5, 2));
+    System.out.println(getBucketCount(2.75, 3.25, 2.5, 1));
+    System.out.println("-----");
+    System.out.println(getBucketCount(-3.4, 2.1, 1.5));
+    System.out.println(getBucketCount(3.4, 2.1, 1.5));
+    System.out.println(getBucketCount(7.25, 4.3, 2.35));
+    System.out.println("-----");
+    System.out.println(getBucketCount(3.4, 1.5));
+    System.out.println(getBucketCount(6.26, 2.2));
+    System.out.println(getBucketCount(3.26, 0.75));
     System.out.println("-----END-----");
+  }
+
+  public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
+    if (width<=0||height<=0||areaPerBucket<=0||extraBuckets<0) {
+      return -1;
+    }
+
+    double areaToPaint = width*height;
+    //System.out.println("areaToPaint: "+areaToPaint);
+    double estimateBuckets = Math.ceil(areaToPaint/areaPerBucket);
+    //System.out.println("estimateBuckets: "+estimateBuckets);
+    int actualBuckets = (int)(estimateBuckets-extraBuckets);
+    //System.out.println("actualBuckets: "+actualBuckets);
+
+    return actualBuckets;
+  }
+
+  public static int getBucketCount(double width, double height, double areaPerBucket) {
+    if (width<=0||height<=0||areaPerBucket<=0) {
+      return -1;
+    }
+
+    double areaToPaint = width*height;
+    //System.out.println("areaToPaint: "+areaToPaint);
+    double estimateBuckets = Math.ceil(areaToPaint/areaPerBucket);
+    //System.out.println("estimateBuckets: "+estimateBuckets);
+    int actualBuckets = (int)estimateBuckets;
+    //System.out.println("actualBuckets: "+actualBuckets);
+
+    return actualBuckets;
+  }
+
+  public static int getBucketCount(double area, double areaPerBucket) {
+    if (area<=0||areaPerBucket<=0) {
+      return -1;
+    }
+
+    double estimateBuckets = Math.ceil(area/areaPerBucket);
+    //System.out.println("estimateBuckets: "+estimateBuckets);
+    int actualBuckets = (int)estimateBuckets;
+    //System.out.println("actualBuckets: "+actualBuckets);
+
+    return actualBuckets;
   }
 }
