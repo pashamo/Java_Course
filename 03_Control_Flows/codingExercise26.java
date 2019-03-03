68892c89490c23c34c3b9c0e86da48805c3a745a
Mohammed Pasha <mk.pasha@hotmail.com>
2019-03-02 20:52:40 -0500

    Update codingExercise26
    replaced "0" with "*"

--- 03_Control_Flows/codingExercise26.java
+++ 03_Control_Flows/codingExercise26.java
@@ -82,7 +82,7 @@ public class codingExercise26 {
       for (int i=0; i<number; i++) {
         for (int j=0; j<number; j++) {
           if (i==0 || i==number-1 || j==0 || j==number-1 || j==i || i==number-j-1) {
-            System.out.print("0");
+            System.out.print("*");
           } else {
             System.out.print(" ");
           }
