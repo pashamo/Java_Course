97be8fb1dee3710e42b52403b16203f80d5bac0d
Mohammed Pasha <mk.pasha@hotmail.com>
2019-03-02 20:44:38 -0500

    codingExercise26 java and class updated
    Makes boxes with a diagonal cross.

--- 03_Control_Flows/codingExercise26.java
+++ 03_Control_Flows/codingExercise26.java
@@ -69,6 +69,26 @@ NOTE: Do not add a main method to the solution code.
 public class codingExercise26 {
   public static void main(String[] args) {
     System.out.println("-----codingExercise26-----");
+    printSquareStar(5);
+    printSquareStar(6);
+    printSquareStar(20);
     System.out.println("-----END-----");
+  }
+
+  public static void printSquareStar(int number) {
+    if (number<5) {
+      System.out.println("Invalid Value");
+    } else {
+      for (int i=0; i<number; i++) {
+        for (int j=0; j<number; j++) {
+          if (i==0 || i==number-1 || j==0 || j==number-1 || j==i || i==number-j-1) {
+            System.out.print("0");
+          } else {
+            System.out.print(" ");
+          }
+        }
+        System.out.print("\n");
+      }
     }
+  }
 }
