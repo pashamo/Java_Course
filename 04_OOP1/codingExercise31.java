e7e7d3e443de1f1ed391b0df4c65f394ee817983
Mohammed Pasha <mk.pasha@hotmail.com>
2019-03-09 11:24:47 -0500

    codingExercise31 updated with classWall

--- 04_OOP1/codingExercise31.java
+++ 04_OOP1/codingExercise31.java
@@ -40,6 +40,57 @@ NOTE: Do not add a main method to the solution code.
 public class codingExercise31 {
   public static void main(String[] args) {
     System.out.println("-----codingExercise31-----");
+
+    Wall wall = new Wall(5,4);
+    System.out.println("area= " + wall.getArea());
+
+    wall.setHeight(-1.5);
+    System.out.println("width= " + wall.getWidth());
+    System.out.println("height= " + wall.getHeight());
+    System.out.println("area= " + wall.getArea());
+
     System.out.println("-----END-----");
+  }
+
+  public static class Wall {
+    private double width;
+    private double height;
+
+    public Wall() {
+
+    }
+    public Wall(double width, double height) {
+      this.width = width;
+      this.height = height;
+
+      if(width < 0) {
+        this.width = 0;
+      } else if (height < 0) {
+        this.height = 0;
+      }
+    }
+    public double getWidth() {
+      return this.width;
+    }
+    public double getHeight() {
+      return this.height;
+    }
+    public void setWidth(double width) {
+      if (width < 0) {
+        this.width = 0;
+      } else {
+        this.width = width;
+      }
+    }
+    public void setHeight(double height) {
+      if (height < 0) {
+        this.height = 0;
+      } else {
+        this.height = height;
+      }
+    }
+    public double getArea() {
+      return this.width*this.height;
+    }
   }
 }
