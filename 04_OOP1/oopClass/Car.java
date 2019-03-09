ab652c2488db771d2eba82cdf6e5c88da5bf019d
Mohammed Pasha <mk.pasha@hotmail.com>
2019-03-09 08:35:28 -0500

    Compiled and executed oopClass and Car files on Windows

--- 04_OOP1/oopClass/Car.java
+++ 04_OOP1/oopClass/Car.java
@@ -13,4 +13,12 @@ public class Car{
     this.engine = "392 Hemi";
     this.colour = "Yellow Jacket";
   }
+
+  public String toString() {
+    return "Doors: "+this.doors+
+    "\nWheels: "+this.wheels+
+    "\nModel: "+this.model+
+    "\nEngine: "+this.engine+
+    "\nColour: "+this.colour;
+  }
 }
--- 04_OOP1/oopClass/oopClass.java
+++ 04_OOP1/oopClass/oopClass.java
@@ -1,9 +1,11 @@
-
-
 public class oopClass {
+
   public static void main(String[] args) {
     System.out.println("-----oopClass-----");
+
     Car dodge = new Car();
+    System.out.println(dodge.toString());
+
     System.out.println("-----END-----");
   }
 }
