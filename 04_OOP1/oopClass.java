e3d67d2cd9900d7d433a4a254027f05d4ea8ed0c
Mohammed Pasha <mk.pasha@hotmail.com>
2019-03-09 07:54:59 -0500

    temporary mid lecture commit, verifying git setup

--- 04_OOP1/oopClass.java
+++ 04_OOP1/oopClass.java
@@ -1,6 +1,18 @@
 public class oopClass {
   public static void main(String[] args) {
     System.out.println("-----oopClass-----");
+    Car dodge = new Car();
     System.out.println("-----END-----");
+  }
+
+  private class Car{
+
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String colour;
+
+
   }
 }
