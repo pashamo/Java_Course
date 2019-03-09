5d824279d6ba5904c69aab420325593e4c8a21ca
Mohammed Pasha <mk.pasha@hotmail.com>
2019-03-09 08:29:02 -0500

    File locations changed

---
+++ 04_OOP1/oopClass/Car.java
@@ -0,0 +1,16 @@
+public class Car{
+
+  private int doors;
+  private int wheels;
+  private String model;
+  private String engine;
+  private String colour;
+
+  Car() {
+    this.doors = 2;
+    this.wheels = 4;
+    this.model = "Challenger";
+    this.engine = "392 Hemi";
+    this.colour = "Yellow Jacket";
+  }
+}
