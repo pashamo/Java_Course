43ecd2ce1a4269c1c2fd1a5c4d59cafd319c156b
Mohammed Pasha <mk.pasha@hotmail.com>
2019-03-09 09:42:42 -0500

    codingExercise30 completed with internal class Person

--- 04_OOP1/codingExercise30.java
+++ 04_OOP1/codingExercise30.java
@@ -53,6 +53,68 @@ NOTE: Do not add the main method to the solution code.
 public class codingExercise30 {
   public static void main(String[] args) {
     System.out.println("-----codingExercise30-----");
+
+    Person person = new Person();
+    person.setFirstName("");   // firstName is set to empty string
+    person.setLastName("");    // lastName is set to empty string
+    person.setAge(10);
+    System.out.println("fullName= " + person.getFullName());
+    System.out.println("teen= " + person.isTeen());
+    person.setFirstName("John");    // firstName is set to John
+    person.setAge(18);
+    System.out.println("fullName= " + person.getFullName());
+    System.out.println("teen= " + person.isTeen());
+    person.setLastName("Smith");    // lastName is set to Smith
+    System.out.println("fullName= " + person.getFullName());
+
     System.out.println("-----END-----");
+  }
+
+  public static class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public String getFirstName(){
+      return this.firstName;
+    }
+    public String getLastName(){
+      return this.lastName;
+    }
+    public int getAge(){
+      return this.age;
+    }
+    public void setFirstName(String firstName){
+      this.firstName = firstName;
+    }
+    public void setLastName(String lastName){
+      this.lastName = lastName;
+    }
+    public void setAge(int age){
+      if (age < 0 && age > 100) {
+        this.age = 0;
+      } else {
+        this.age = age;
+      }
+    }
+    public boolean isTeen(){
+      if (this.age > 12 && this.age < 20) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    public String getFullName(){
+      if (this.firstName.isEmpty() && this.lastName.isEmpty()) {
+        return "";
+      } else if (this.firstName.isEmpty()) {
+        return this.lastName;
+      } else if (this.lastName.isEmpty()) {
+        return this.firstName;
+      } else {
+        return this.firstName + " " + this.lastName;
+      }
+    }
+
   }
 }
