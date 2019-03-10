352c1920f0a00f78b75aec17ad7de4e3f17bc384
Mohammed Pasha <mk.pasha@hotmail.com>
2019-03-09 21:26:00 -0500

    codingExcercise32 completed with Point class

--- 04_OOP1/codingExercise32.java
+++ 04_OOP1/codingExercise32.java
@@ -53,6 +53,50 @@ NOTE: Do not add a main method to the solution code.
 public class codingExercise32 {
   public static void main(String[] args) {
     System.out.println("-----codingExercise32-----");
+
+    Point first = new Point(6, 5);
+    Point second = new Point(3, 1);
+    System.out.println("distance(0,0)= " + first.distance());
+    System.out.println("distance(second)= " + first.distance(second));
+    System.out.println("distance(2,2)= " + first.distance(2, 2));
+    Point point = new Point();
+    System.out.println("distance()= " + point.distance());
+
     System.out.println("-----END-----");
+  }
+
+
+  public static class Point {
+    private int x;
+    private int y;
+
+    public Point() {}
+    public Point(int x, int y) {
+      this.x = x;
+      this.y = y;
+    }
+    
+    public int getX() {
+      return this.x;
+    }
+    public int getY() {
+      return this.y;
+    }
+    public void setX(int x) {
+      this.x = x;
+    }
+    public void setY(int y) {
+      this.y = y;
+    }
+    //d(A,B)=√ (xB − xA) * (xB - xA) + (yB − yA) * (yB - yA)
+    public double distance() {
+      return distance(0,0);
+    }
+    public double distance(int x, int y) {
+      return (double)Math.sqrt((x - this.x) * (x - this.x) + (y - this.y) * (y - this.y));
+    }
+    public double distance(Point another) {
+      return distance(another.getX(), another.getY());
+    }
   }
 }
