c584b62584830cf055fe6d01cbc14da6d34514f2
Mohammed Pasha <mk.pasha@hotmail.com>
2019-03-10 16:02:45 -0400

    skeleton code for all lectures in this section

---
+++ 05_OOP2/oopEncapsulation.java
@@ -0,0 +1,9 @@
+public class oopEncapsulation {
+  public static void main(String[] args){
+    System.out.println("-----oopEncapsulation-----");
+
+
+
+    System.out.println("-----END-----");
+  }
+}
---
+++ 05_OOP2/oopMasterChallenge.java
@@ -0,0 +1,9 @@
+public class oopMasterChallenge {
+  public static void main(String[] args){
+    System.out.println("-----oopMasterChallenge-----");
+
+
+
+    System.out.println("-----END-----");
+  }
+}
