27f01bde72e598a1e714aae5f64daf24f02a4d93
Mohammed Pasha <mk.pasha@hotmail.com>
2019-03-12 19:28:13 -0400

    midd work save, challenge testing

--- 06_Arrays_Lists_Boxing/autoboxUnboxChallenge.java
+++ 06_Arrays_Lists_Boxing/autoboxUnboxChallenge.java
@@ -28,11 +28,118 @@ public class autoboxUnboxChallenge {
   public static void main(String[] args) {
     System.out.println("-----autoboxUnboxChallenge-----");
 
-
+    Customer customer1 = new Customer("Mohammed", 1000.00);
+    customer1.addTransaction(-11.99);
+    Customer customer2 = new Customer("Abdullah", 2000.00);
+    customer2.addTransaction(-149.99);
+    System.out.println(customer1.toString());
+    System.out.println(customer2.toString());
+    Branch branch = new Branch(12345);
+    branch.addCustomer(customer1);
+    branch.addCustomer(customer2);
+    System.out.println(branch.toString());
 
     System.out.println("-----END-----");
   }
 
 
+  public static class Bank {
+    private ArrayList<Branch> branches;
+
+    public Bank() {
+      branches = new ArrayList<Branch>();
+    }
+    public ArrayList<Branch> getBranches() {
+      return branches;
+    }
+    public void setBranches(ArrayList<Branch> branches) {
+      this.branches = branches;
+    }
+    public void addBranch(Branch br) {
+      this.branches.add(br);
+    }
+
+  }
+
+
+  public static class Branch {
+    private int brNumber;
+    private ArrayList<Customer> customers;
+
+    public Branch(int brNumber) {
+      this.brNumber = brNumber;
+      customers = new ArrayList<Customer>();
+    }
+    public int getBrNumber() {
+      return this.brNumber;
+    }
+    public void setBrNumber(int brNumber) {
+      this.brNumber = brNumber;
+    }
+    public ArrayList<Customer> getCustomers() {
+      return customers;
+    }
+    public void setCustomers(ArrayList<Customer> customers) {
+      this.customers = customers;
+    }
+    public void addCustomer(Customer customer) {
+      this.customers.add(customer);
+    }
+    public void addCustomerTransaction(String name, double trns) {
+      for(int i = 0; i < customers.size(); i++) {
+        if (this.customers.get(i).getCustName().equals(name)) {
+          Customer temp = this.customers.get(i);
+          temp.addTransaction(Double.valueOf(trns));
+          this.customers.set(i, temp);
+          System.out.println("Transaction for customer added");
+        }
+      }
+      System.out.println("Transaction not added");
+    }
+    public boolean isCustomer(String name) {
+      return true;
+    }
+    public String toString() {
+      String temp = "Branch#"+brNumber+"\n";
+      for(int i = 0; i<customers.size(); i++) {
+        temp += i+".\t" + customers.get(i).toString() + "\n";
+      }
+      return temp;
+    }
+  }
+
+
+  public static class Customer {
+    private String custName;
+    private ArrayList<Double> transactions;
+
+    public Customer(String custName, double initTransaction) {
+      this.custName = custName;
+      transactions = new ArrayList<Double>();
+      transactions.add(Double.valueOf(initTransaction));
+    }
+    public String getCustName() {
+      return this.custName;
+    }
+    public void setCustname(String custName) {
+      this.custName = custName;
+    }
+    public ArrayList<Double> getTransactions() {
+      return transactions;
+    }
+    public void setTransactions(ArrayList<Double> transactions) {
+      this.transactions = transactions;
+    }
+    public void addTransaction(double trns) {
+      this.transactions.add(Double.valueOf(trns));
+    }
+    public String toString() {
+      String temp = custName + "\n";
+      for(int i = 0; i<transactions.size(); i++) {
+        temp += "\t" + transactions.get(i).doubleValue() + "\n";
+      }
+      return temp;
+    }
+  }
 
 }
